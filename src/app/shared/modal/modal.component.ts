5ce43d45d7cc7fff7d179fb9bb93745f59cc8c91
Marcos Vidal <marcosvidal@outlook.com>
2022-11-02 16:03:09 -0300

    desacoplamento modal para impedir css de fazer mudança

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ElementRef } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -9,9 +9,14 @@ import { ModalService } from 'src/app/services/modal.service';
 export class ModalComponent implements OnInit {
   @Input() modalID = '';
 
-  constructor(public modal: ModalService) {}
+  //Elementref, para remover o conteudo do modal e isolar para evitar alterações do css
 
-  ngOnInit(): void {}
+  constructor(public modal: ModalService, public el: ElementRef) {}
+
+  ngOnInit(): void {
+    //solicitando o desacoplamento do modal durante a inicialização do modulo.
+    document.body.appendChild(this.el.nativeElement);
+  }
 
   closeModal() {
     this.modal.toggleModal(this.modalID);
